9cd404144fa423d4918df5ff9066615803862db2
Yongho Kim <ironyonghokim@gmail.com>
2023-06-19 23:12:07 -0500

    Added psutil to find and access plugin cgroup

--- pkg/controller/plugincontroller.go
+++ pkg/controller/plugincontroller.go
@@ -1,8 +1,13 @@
 package controller
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/waggle-sensor/edge-scheduler/pkg/datatype"
 	"github.com/waggle-sensor/edge-scheduler/pkg/logger"
+
+	"github.com/shirou/gopsutil/v3/process"
 )
 
 // this program prints out logs of performance, plugin stdout/stderr, current control params, events
@@ -13,6 +18,7 @@ import (
 type ControllerConfig struct {
 	EnableCPUPerformanceLogging bool
 	EnableGPUPerformanceLogging bool
+	PluginProcessName           string
 	AppCgroupDir                string
 	GPUMetricHost               string
 }
@@ -28,9 +34,34 @@ func NewController(c ControllerConfig) *Controller {
 }
 
 func (c *Controller) Run() {
+	logger.Info.Println("plugin controller started.")
 	ch := make(chan datatype.Event)
+	pids, _ := process.Pids()
+	var pluginProc *process.Process
+	logger.Info.Printf("looking for the plugin process (%s) ...", c.config.PluginProcessName)
+	for _, pid := range pids {
+		if p, err := process.NewProcess(pid); err != nil {
+			logger.Error.Printf("failed to get process %d: %s", pid, err.Error())
+		} else {
+			if pName, err := p.Name(); err == nil {
+				logger.Info.Printf("pid %d (%s) found", pid, pName)
+				if c.config.PluginProcessName == pName {
+					pluginProc = p
+					break
+				}
+			}
+		}
+	}
+	if pluginProc == nil {
+		panic(fmt.Sprintf("failed to find the plugin process (%s)", c.config.PluginProcessName))
+	}
 	if c.config.EnableCPUPerformanceLogging {
 		logger.Info.Println("CPU performance measurement enabled")
+		if c.config.AppCgroupDir == "" {
+			logger.Info.Println("plugin cgroup directory is not given.")
+			c.config.AppCgroupDir = fmt.Sprintf("/proc/%d/root/sys/fs/cgroup", pluginProc.Pid)
+			logger.Info.Printf("plugin cgroup path found: %s", c.config.AppCgroupDir)
+		}
 		p := NewCPUPerformanceLogging(c.config)
 		p.Notifier.Subscribe(ch)
 		go p.Run()
@@ -41,9 +72,22 @@ func (c *Controller) Run() {
 		g.Notifier.Subscribe(ch)
 		go g.Run()
 	}
-
-	for e := range ch {
-		data, _ := e.EncodeMetaToJson()
-		logger.Info.Printf("%s: %s", e.ToString(), data)
+	ticker := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			if pluginPidExists, err := process.PidExists(pluginProc.Pid); err == nil {
+				if !pluginPidExists {
+					logger.Info.Printf("plugin's PID (%d) does not exist", pluginProc.Pid)
+					logger.Info.Println("plugin is terminated. plugin-controller terminates successfully.")
+					return
+				}
+			} else {
+				logger.Error.Printf("failed to probe plugin PID (%d): %s", pluginProc.Pid, err.Error())
+			}
+		case e := <-ch:
+			data, _ := e.EncodeMetaToJson()
+			logger.Info.Printf("%s: %s", e.ToString(), data)
+		}
 	}
 }
